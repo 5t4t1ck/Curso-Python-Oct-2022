cbcd579cbfd6846d07827ed04b1bf9cbec90e156
Diego Saavedra <dsaavedra88@gmail.com>
2022-11-02 20:48:03 -0500

    Ejemplos de Funciones

---
+++ Funciones/ejemplo01.py
@@ -0,0 +1,4 @@
+def saludar(nombre):
+    return f"Hola {nombre}, ¿como estás?"
+
+print(saludar("Diego Saavedra"))
---
+++ Funciones/ejemplo02.py
@@ -0,0 +1,7 @@
+#import ejemplo01
+#from ejemplo01 import saludar
+from ejemplo01 import saludar as s
+
+#print(ejemplo01.saludar("Diego"))
+#print(saludar("Diego"))
+print(s("Diego"))
